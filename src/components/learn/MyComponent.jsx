516f18d2d484cefd1094f76065bec5ad8608cbcb
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-11 23:32:32 +0700

    video 31 - bien so

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,20 @@
 import "./style.css";
 
 const MyComponent = () => {
+  //const hoidanit = "eric"; //string
+  // const hoidanit = 25; //number
+  // const hoidanit = true; //boolean
+  //const hoidanit = undefined;
+  //const hoidanit = null;
+  const hoidanit = [1, 2, 3];
+  // const hoidanit = {
+  //   name: "hoidanit",
+  //   age: 25,
+  // };
   return (
     <>
-      <div>eric & hoidanit update</div>
+      <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+      <div>{console.log("ERIC")}</div>
       <div className="child" style={{borderRadius: "10px"}}>
         child
       </div>
