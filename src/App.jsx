098b0b4052dfb66c2b3ca1763580a1f079bc0113
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-13 10:16:58 +0700

    video 40 bổ sung key

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,9 @@ import {useState} from "react";
 
 const App = () => {
   const [todoList, setTodoList] = useState([
-    {id: 1, name: "Learning React"},
-    {id: 2, name: "Watching Youtube"},
+    // {id: 1, name: "Learning React"},
+    // {id: 2, name: "Watching Youtube"},
   ]);
-
-  const hoidanit = "Eric Argentina";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam",
-  };
 
   const addNewToDo = (name) => {
     const newTodo = {
@@ -40,7 +33,7 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewToDo={addNewToDo} />
-      <TodoData name={hoidanit} age={age} data={data} todoList={todoList} />
+      <TodoData todoList={todoList} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
