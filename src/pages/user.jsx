adbf7143cff0a4f49a09620f87677c800732d737
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-14 09:45:28 +0700

    video 66 Tạo base giao diện User

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,5 +1,13 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UserPage = () => {
-  return <div>users page</div>;
+  return (
+    <div style={{padding: "20px"}}>
+      <UserForm />
+      <UserTable />
+    </div>
+  );
 };
 
 export default UserPage;
