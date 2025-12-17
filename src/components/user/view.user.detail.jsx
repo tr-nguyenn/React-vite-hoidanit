28cecb70be71797d09edd06f984383298b6e1958
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-17 22:07:39 +0700

    video 82 - display avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,10 +1,11 @@
-import {Drawer} from "antd";
+import {Button, Drawer} from "antd";
 
 const ViewUserDetail = (props) => {
   const {dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen} = props;
   return (
     <>
       <Drawer
+        width={"40vw"}
         title="Chi tiết user"
         closable={{"aria-label": "Close Button"}}
         onClose={() => {
@@ -23,6 +24,33 @@ const ViewUserDetail = (props) => {
             <br />
             <p>Phone: {dataDetail.phone}</p>
             <br />
+            <p>Avatar: </p>
+            <br />
+            <div>
+              <img
+                height={100}
+                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                  dataDetail.avatar
+                }`}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="btnUpload"
+                style={{
+                  display: "block",
+                  width: "fit-content",
+                  marginTop: "15px",
+                  padding: "5px 10px",
+                  background: "orange",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+              >
+                Upload Avatar
+              </label>
+              <input type="file" hidden id="btnUpload" />
+            </div>
           </>
         ) : (
           <>
